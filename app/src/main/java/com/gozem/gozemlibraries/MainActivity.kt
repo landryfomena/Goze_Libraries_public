5f14a934b57f714daa1f5c3379f828cee48e16fd
landrykemtsop <landry.fomena@gozem.co>
2022-09-16 11:06:54 +0200

    Initial commit

---
+++ app/src/main/java/com/gozem/gozemlibraries/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.gozem.gozemlibraries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gozem.gozemlibraries.ui.theme.GozemLibrariesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GozemLibrariesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GozemLibrariesTheme {
+        Greeting("Android")
+    }
+}
